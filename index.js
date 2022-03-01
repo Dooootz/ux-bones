9d3461edbf3beec725393edb26af987d80e9e0a8
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-01 22:25:06 +1000

    need to fetch top & bottom nav

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ const test = () => console.log('test click')
 
 const createProfilePage = () => window.location.pathname = '/src/pages/createProfile.html'
 
-const profilePage = () => window.location.pathname = '/src/pages/profilePage.html'
+const profilePage = () => window.location.pathname = '/src/pages/profile.html'
+
+const notificationsPage = () => window.location.pathname = '/src/pages/notifications.html'
+
+const chatPage = () => window.location.pathname = '/src/pages/chat.html'
+
+const searchPage = () => window.location.pathname = '/src/pages/search.html'
+
+const friendsPage = () => window.location.pathname = '/src/pages/friends.html'
+
+const userSettingsPage = () => window.location.pathname = '/src/pages/userSettings.html'
